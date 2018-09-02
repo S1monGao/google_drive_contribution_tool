fe004fc623e07551bcb5b872f25ab60a032774ae
Keith Pang <kpan0004@student.monash.edu>
2018-09-02 22:48:51 +1000

    Measuring characters
    File now plots pie and line charts based on number of characters added and deleted

--- keith/v2_test.py
+++ keith/v2_test.py
@@ -2,10 +2,9 @@ from __future__ import print_function
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
-import urllib
 import datetime as dt
 import matplotlib.pyplot as plt
-from classes import User
+from classes import User_v2, Revision
 
 
 # If modifying these scopes, delete the file token.json.
@@ -13,62 +12,16 @@ SCOPES = 'https://www.googleapis.com/auth/drive.metadata.readonly https://www.go
 start_time = dt.datetime(2018, 8, 29)
 end_time = dt.datetime.now()
 
+# Authenticating and creating service
+store = file.Storage('token.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('drive', 'v2', http=creds.authorize(Http()))
+
 
 def main():
-
-    # Authenticating and creating service
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('drive', 'v2', http=creds.authorize(Http()))
-
-    # Call the Drive v3 API
-    results = service.files().list().execute()
-    items = results['items']
-
-    if not items:
-        print('No files found.')
-    else:
-        for item in items:
-            if item['title'] == "test_non_google_doc":
-                print('{0} ({1})'.format(item['title'], item['id']))
-                revisions = service.revisions().list(fileId=item['id'], fields="*").execute()
-                users = [] #Will store a list of user class instances
-                for revision in revisions['items']:
-                    print(revision)
-                    modifier = revision['lastModifyingUser']
-                    modifier_name = modifier['displayName'] #Get the name of the person who made the modification
-                    modified_time = dt.datetime.strptime(revision['modifiedDate'][:-2], '%Y-%m-%dT%H:%M:%S.%f') # convert time of conversion to dt object
-                    modified_time = modified_time + dt.timedelta(hours=10) # Hardcoded conversion to AEST
-                    modifier_found = False
-                    for user in users:
-                        if user.name == modifier_name:
-                            user.times.append(modified_time)
-                            user.num_rev += 1
-                            modifier_found = True
-                    if not modifier_found:
-                        new_user = User(name=modifier_name)
-                        new_user.times.append(modified_time)
-                        new_user.num_rev += 1
-                        users.append(new_user)
-                for user in users:
-                    print(user)
-                plot_pie_chart(users)
-                plot_lines(users, start_time, end_time)
-                break
-
-
-def main2():
-
-    # Authenticating and creating service
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
-        creds = tools.run_flow(flow, store)
-    service = build('drive', 'v2', http=creds.authorize(Http()))
 
     # Call the Drive v3 API
     results = service.files().list().execute()
@@ -81,16 +34,41 @@ def main2():
             if item['title'] == "Test Doc":
                 print('{0} ({1})'.format(item['title'], item['id']))
                 revisions = service.revisions().list(fileId=item['id'], fields="*").execute()
+                users = [] #Will store a list of user class instances
+                previous_revision_num_chars = 0
                 for revision in revisions['items']:
-                    revision_object = service.revisions().get(fileId=item['id'], revisionId=revision['id']).execute()
-                    text_link = revision_object['exportLinks']['text/plain']
-                    _, data = service._http.request(text_link)
-                    data = str(data, 'utf-8')
-                    print(data)
-                    print('-------')
+                    revision_content = get_revision_content(revision)
+                    num_chars = len(revision_content) - previous_revision_num_chars
+                    is_addition = False
+                    if num_chars > 0:
+                        is_addition = True
+                    num_chars = abs(num_chars)
+                    previous_revision_num_chars = len(revision_content)
 
-
+                    modifier = revision['lastModifyingUser']
+                    modifier_name = modifier['displayName'] #Get the name of the person who made the modification
+                    modified_time = dt.datetime.strptime(revision['modifiedDate'][:-2], '%Y-%m-%dT%H:%M:%S.%f') # convert time of conversion to dt object
+                    modified_time = modified_time + dt.timedelta(hours=10) # Hardcoded conversion to AEST
+                    modifier_found = False
+                    revision_object = Revision(num_chars, modified_time, is_addition)
+                    for user in users:
+                        if user.name == modifier_name:
+                            user.add_revision(revision_object)
+                            modifier_found = True
+                    if not modifier_found:
+                        new_user = User_v2(name=modifier_name)
+                        new_user.add_revision(revision_object)
+                        users.append(new_user)
+                plot_pie_chart(users)
+                plot_lines(users, start_time, end_time, True)
+                plot_lines(users, start_time, end_time, False)
                 break
+
+def get_revision_content(revision):
+    text_link = revision['exportLinks']['text/plain']
+    _, data = service._http.request(text_link)
+    data = str(data, 'utf-8')
+    return "".join(data.split())
 
 
 def plot_pie_chart(users):
@@ -100,36 +78,38 @@ def plot_pie_chart(users):
     :return: None
     :postcondition: pie chart for number of revisions per user will be plotted
     """
-    amounts = [user.num_rev for user in users]
+    amounts = [user.num_added - user.num_deleted for user in users]
     labels = [user.name for user in users]
     plt.pie(amounts, labels=labels)
     plt.title("Work ratio of users")
     plt.show()
 
 
-def plot_line_info(user, start_time, end_time, num_sections):
+def plot_line_info(user, start_time, end_time, num_sections, is_additions):
     """
 
-    :param user: A User class instance
-    :param start_time: Time at which to start measuring changes
-    :param end_time: Time at which to stop measuring changes
-    :param num_sections: The number of 6hr slots between start and end
+    :param user (User_v2 instance): A User class instance
+    :param start_time (datetime): Time at which to start measuring changes
+    :param end_time (datetime): Time at which to stop measuring changes
+    :param num_sections (integer): The number of 6hr slots between start and end
+    :param is_additions (boolean): Measuring additions or deletions
     :return: A list containing the number of revisions made by the user in each 6hr interval
     """
     num_revs_list = num_sections * [0]
     current_time = start_time
-    times = user.times
+    revisions = [revision for revision in user.revisions]
     index = 0
     while current_time < end_time:
-        while len(times) > 0 and current_time <= times[0] <= current_time + dt.timedelta(hours=6):
-            num_revs_list[index] += 1
-            times.pop(0)
+        while len(revisions) > 0 and current_time <= revisions[0].time <= current_time + dt.timedelta(hours=6):
+            if revisions[0].is_add is is_additions:
+                num_revs_list[index] += revisions[0].num_chars
+            revisions.pop(0)
         current_time += dt.timedelta(hours=6)
         index += 1
     return num_revs_list
 
 
-def plot_lines(users, start_time, end_time):
+def plot_lines(users, start_time, end_time, is_additions):
     """
     Plots the line graphs for number of revisions for each user at each time slot
     :param users: A list of User class instances
@@ -144,12 +124,15 @@ def plot_lines(users, start_time, end_time):
     for i in range(1, num_sections):
         time_axis[i] = start_time + i*dt.timedelta(hours=6)
     for user in users:
-        plt.plot(time_axis, plot_line_info(user, start_time, end_time, num_sections))
+        plt.plot(time_axis, plot_line_info(user, start_time, end_time, num_sections, is_additions))
     plt.legend([user.name for user in users], loc='upper right')
     plt.xlabel("Date and hour")
-    plt.ylabel("Number of revisions")
-    plt.title("Revisions for each user at given times")
+    plt.ylabel("Number of characters")
+    if is_additions:
+        plt.title("Number of characters added by each user at given times")
+    else:
+        plt.title("Number of characters deleted by each user at given times")
     plt.show()
 
 if __name__ == '__main__':
-    main2()
+    main()
